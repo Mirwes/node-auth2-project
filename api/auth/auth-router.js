22851fec5a7b17553ffedf171a5a26a94228a808
Mirwes <mirwes210@gmail.com>
2021-03-14 23:15:43 -0700

    code added to auth-router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { JWT_SECRET } = require("../secrets/index.js"); // use this secret!
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Users = require("../users/users-model");
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
     response:
     status 201
     {
@@ -14,29 +16,59 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      if(saved)
+        res.status(201).json(saved);
+      else 
+        res.json({"message" : "Could not add user"});
+    })
 });
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
     response:
     status 200
     {
       "message": "sue is back!",
       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
     }
-
     The token must expire in one day, and must provide the following information
     in its payload:
-
     {
       "subject"  : 1       // the user_id of the authenticated user
       "username" : "bob"   // the username of the authenticated user
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  const { username, password } = req.body;
+  Users.findBy({username}).first()
+    .then(user => {
+      const validPass = bcrypt.compareSync(password, user.password);
+      if(!validPass)
+        return res.status(401).json({message: "Invalid Credentials"})
+      else {
+        const token = generateToken(user);
+        res.cookie("token", token)
+        res.json({"message":`Welcome ${user.username}`});
+      }
+    })
 });
 
+function generateToken(user){
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload,JWT_SECRET, options)
+}
 module.exports = router;
